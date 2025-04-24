f812fa2380df1059914abcf3ef07be32ab551399
Gustavo Goncalves <gustavo.goncalves@optsol.com.br>
2025-04-24 17:11:46 -0300

    projeto inicial com estrutura base criada

---
+++ api-clean-architecture/CleanArchMvc.WebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanArchMvc.WebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
