84a07bac5162948fb9954c9734af14d6430ce3d2
Gustavo Goncalves <gustavo.goncalves@optsol.com.br>
2025-04-29 17:33:32 -0300

    Algumas propriedades foram renomeadas.

--- CleanArchitecture.Domain/Entities/Product.cs
+++ CleanArchitecture.Domain/Entities/Product.cs
@@ -14,5 +14,8 @@ namespace CleanArchitecture.Domain.Entities
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public string Image { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
