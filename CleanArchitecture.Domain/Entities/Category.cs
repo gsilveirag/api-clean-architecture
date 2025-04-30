5b06e10d685341e52bb9e6d1792adce5bbdfdd70
Gustavo Silveira Gonçalves <gustavo.goncalves@optsolv.com.br>
2025-04-30 18:06:29 -0300

    Criando as interfaces e entidade bases

--- CleanArchitecture.Domain/Entities/Category.cs
+++ CleanArchitecture.Domain/Entities/Category.cs
@@ -7,33 +7,35 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecture.Domain.Entities
 {
-    public class Category
+    public sealed class Category : Entity
     {
-        public Guid Id { get; private set; }
         public string Name { get; private set; }
 
         public Category(string name)
         {
-            ValidationDomain(name);
+            ValidateDomain(name);
+        }
+        public ICollection<Product> Products { get; set; }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required.");
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short.");
+
+            Name = name;
         }
 
         //So foi criado para popular a tabela via codigo.
         public Category(Guid id, string name)
         {
             Id = id;
-            Name = name; 
-        }
-        public ICollection<Product> Products { get; set; }
-
-
-        private void ValidationDomain(string name)
-        {
-            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
-                "Invalid name.Name is required.");
-            DomainExceptionValidation.When(name.Length < 3,
-                "Invalid name, too short.");
-
-            Name = name;
+            ValidationDomain(name);
         }
     }
 }
--- CleanArchitecture.Domain/Entities/Product.cs
+++ CleanArchitecture.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitecture.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +7,56 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecture.Domain.Entities
 {
-    public class Product
+    public sealed class Product : Entity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-        public string Image { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+
+        //Construtor: garantir que certos dados sejam obrigatórios ou inicializar propriedades corretamente.
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+            ValidateDomain(name, description, price, stock, image);
+            CategoryId = categoryId;
+        }
+
+        private void ValidateDomain (string name, string description, decimal price, int stock, string image) 
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required.");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name. Name is too short.");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid name. Description is required.");
+            DomainExceptionValidation.When(description.Length < 3, "Invalid name. Name is too short.");
+
+            DomainExceptionValidation.When(price < 0, "Invalid price value.");
+
+            DomainExceptionValidation.When(stock < 0, "Invalid stock value.");
+
+            DomainExceptionValidation.When(image.Length > 250, "Invalid image. Image is too long.");
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
 
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+
+
+        //So foi criado para popular a tabela via codigo.
+        public Product(Guid id, string name, string description, decimal price, int stock, string image)
+        {
+            Id = id;
+            ValidateDomain(name, description, price, stock, image);
+        }
     }
 }
