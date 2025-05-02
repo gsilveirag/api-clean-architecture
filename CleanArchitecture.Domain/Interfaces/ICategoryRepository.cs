c3e9fd6aa790eefd763eea9002fbeae6252fc792
Gustavo Silveira Gonçalves <gustavo.goncalves@optsolv.com.br>
2025-05-02 11:35:33 -0300

    implementacoes das interfaces

--- CleanArchitecture.Domain/Interfaces/ICategoryRepository.cs
+++ CleanArchitecture.Domain/Interfaces/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitecture.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecture.Domain.Interfaces
 {
-    internal interface ICategoryRepository
+    public interface ICategoryRepository
     {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetById(Guid id);
+
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
     }
 }
